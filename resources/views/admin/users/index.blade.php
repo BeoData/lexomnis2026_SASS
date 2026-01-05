629445ea790834082b2d7bc093236a80180e8abe
beodata <office@beodata.rs>
2026-01-05 11:40:56 +0100

    Fix pagination links - Move label inside link/span tags

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -83,13 +83,15 @@
                             <a
                                 href="{{ $link['url'] }}"
                                 class="px-3 py-2 border border-gray-300 rounded-md text-sm font-medium {{ $link['active'] ? 'bg-blue-600 text-white' : 'bg-white text-gray-700 hover:bg-gray-50' }}"
-                                {!! $link['label'] !!}
                             >
+                                {!! $link['label'] !!}
+                            </a>
                         @else
                             <span
                                 class="px-3 py-2 border border-gray-300 rounded-md text-sm font-medium opacity-50 cursor-not-allowed"
-                                {!! $link['label'] !!}
                             >
+                                {!! $link['label'] !!}
+                            </span>
                         @endif
                     @endforeach
                 </div>
